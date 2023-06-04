2195c71ddac92abfb37fbc5d10d096ce84be04df
johnr21 <johnrabelo.contact@gmail.com>
2023-06-03 21:44:46 -0300

    indexElement

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Bookpg/>} />
+        <Route index element={<Bookpg/>} />
         <Route path='/thankyou' element={<ThankYouPg/>} />
       </Routes>
     </div>
